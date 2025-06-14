f18a8d9a9339efa91a61ce9ffcd24e0c6899a3d5
RitoxicGame <bigmagicrat@gmail.com>
2025-06-14 12:42:37 -0400

    Added Steppy Sound(s), Recorded Voiceover
    Steppy sounds are fully implemented (as well as can be at this point 😭)
    
    Voiceover has been recorded, but not implemented.
    
    Also added the Reaper project file(s), b/c am overly anxious 💀

--- Scripts/Character/Player.gd
+++ Scripts/Character/Player.gd
@@ -3,9 +3,16 @@ extends CharacterBody3D
 @export var mouse_sensitivity := 0.002
 @export var move_speed := 3.0
 
+@export var step_sound_distance_interval := .25
+@export var pitch_variance := 0.04
+@export var step_SFX: AudioStream
+
 var _look_vertical := 0.0
+var distance_since_SFX_played := 0.0
+var rand_pitch_shift:= 1.0
 
 @onready var head := $Head
+@onready var sfx_player := $SFXPlayer
 
 var myPauseMenu = preload("res://Scenes/pause_menu.tscn")
 var subscene_instance: Node = null
@@ -49,7 +56,23 @@ func _physics_process(delta):
 	
 	velocity = input_dir * move_speed
 	move_and_slide()
+	
+	distance_since_SFX_played += velocity.length() * get_process_delta_time()
+	if(distance_since_SFX_played >= step_sound_distance_interval):
+		_play_steppy()
+	
 
 func _on_pause_menu_closed():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	subscene_instance = null
+
+func _play_steppy():
+	distance_since_SFX_played = 0
+	if(!sfx_player.stream):
+		sfx_player.stream = step_SFX
+	
+	rand_pitch_shift = (1.0 + (pitch_variance / 2.0)) - (randf() * pitch_variance)
+	sfx_player.pitch_scale = rand_pitch_shift
+	
+	sfx_player.play()
+	
