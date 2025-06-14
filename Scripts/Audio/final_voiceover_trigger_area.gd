ae2f0986888fb5fa44b9585b9397f88b347e372c
Kukeymonster <itpm25@gmail.com>
2025-06-14 16:04:21 -0400

    final final push

--- Scripts/Audio/final_voiceover_trigger_area.gd
+++ Scripts/Audio/final_voiceover_trigger_area.gd
@@ -9,12 +9,14 @@ func _kill_me_now() -> void:
 	process_mode = Node.PROCESS_MODE_DISABLED
 
 func _on_final_voiceover_playback_finished():
-	await 1.5
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
 
 func _on_final_body_entered(body):
-	body.set_process_input(false)
+	myPlayer.set_process_input(false)
+	myPlayer.velocity = Vector3.ZERO
+	myPlayer.process_mode = Node.PROCESS_MODE_DISABLED
+	myPlayer.set_physics_process(false)
 	audio_stream_player.stream = voiceover_audio
 	call_deferred("_kill_me_now")
 	audio_stream_player.play()
